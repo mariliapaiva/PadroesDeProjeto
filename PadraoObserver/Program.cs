33694b2215f6d13fc2e601e5cfb7ee94e4651406
Alberto Monteiro <alberto.monteiro@live.com>
2015-03-27 19:23:20 -0300

    Commit inicial

---
+++ PadraoObserver/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PadraoObserver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    interface IObserver {
+        void Update();
+    }
+    interface ISubject {
+        void Attach( IObserver observer);
+        void Dettach( IObserver observer);
+        void DettachAll();
+        void Notify();
+
+    }
+
+    class ObserverBase : IObserver
+    {
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class SubjectBase : ISubject
+    {
+        public void Attach(IObserver observer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dettach(IObserver observer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DettachAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Notify()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
